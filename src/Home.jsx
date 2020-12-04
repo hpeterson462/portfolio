8bae6c1f33b880c954a2a35cd8b908506265fa49
Heather Peterson <hpeterson462@gmail.com>
2020-12-04 11:15:16 -0800

    skeleton setup

---
+++ src/Home.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Heather Peterson</h1>
+        <h3>Fullstack Software Developer</h3>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            Projects
+          </li>
+          <li>
+            About Me
+          </li>
+          <li>
+            Contact
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <div>
+          <h3>Projects</h3>
+        </div>
+        <div>
+          <h3>About Me</h3>
+          <p>
+            I’m a full-stack software developer with a passion for solving human-based problems, collaborating with a great team, and the great outdoors. On previous projects, I’ve used test-driven development to create PostgreSQL databases using RESTful APIs that distill information down into JavaScript and React user interfaces with CSS and Sass designs. I’m excited to continue discovering, learning, and working with amazing people.
+            </p>
+          <section>
+            <p>Tech Stack</p>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </section>
+        </div>
+        <div>
+          <h3>Contact</h3>
+          <p>Get in touch!</p>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <p>/hpeterson462</p>
+          <img src="" alt="" />
+          <p>Resume</p>
+        </div>
+      </main>
+    </div>
+  )
+}
