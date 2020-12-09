113c02148c20d5abac7195881c00be95431c3cb2
Heather Peterson <hpeterson462@gmail.com>
2020-12-08 20:32:18 -0800

    link colors

--- src/menu/BurgerMenu.jsx
+++ src/menu/BurgerMenu.jsx
@@ -4,7 +4,7 @@ import { TiThMenu } from "react-icons/ti";
 export default function BurgerMenu({ open, setOpen }) {
   return (
     <div>
-      <TiThMenu open={open} onClick={() => setOpen(!open)} size={20} className="cursor-pointer m-3" />
+      <TiThMenu open={open} onClick={() => setOpen(!open)} size={20} className="cursor-pointer m-3 hover:bg-gray-200 rounded-sm" />
     </div>
   )
 }
--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Menu({ open }) {
   return (
-    <div open={open} className="flex flex-col bg-gray-500 bg-opacity-5 p-20 w-3/6 absolute">
-      <a href="/" className="p-5">
+    <div open={open} className="flex flex-col bg-gray-500 bg-opacity-5 p-20 w-3/6 rounded-sm absolute">
+      <a href="/" className="p-5 rounded-sm hover:text-white hover:bg-gray-900 hover:bg-opacity-20">
         Home
     </a>
-      <a href="/projects" className="p-5">
+      <a href="/projects" className="p-5 rounded-sm hover:text-white hover:bg-gray-900 hover:bg-opacity-20">
         Projects
     </a>
-      <a href="/aboutme" className="p-5">
+      <a href="/aboutme" className="p-5 rounded-sm hover:text-white hover:bg-gray-900 hover:bg-opacity-20">
         About Me
     </a>
-      <a href="/contact" className="p-5">
+      <a href="/contact" className="p-5 rounded-sm hover:text-white hover:bg-gray-900 hover:bg-opacity-20">
         Contact
     </a>
     </div>
