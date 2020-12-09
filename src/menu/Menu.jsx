18ae7d2416b601ab6428683472ff1473cf636f95
Heather Peterson <hpeterson462@gmail.com>
2020-12-08 19:43:14 -0800

    menu links column direction, bg opactiy, width

--- src/menu/Menu.jsx
+++ src/menu/Menu.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function Menu({ open }) {
   return (
-    <div open={open} className="flex justify-evenly">
-      <a href="/">
+    <div open={open} className="flex flex-col bg-gray-500 bg-opacity-5 p-20 w-3/6 absolute">
+      <a href="/" className="p-5">
         Home
     </a>
-      <a href="/projects">
+      <a href="/projects" className="p-5">
         Projects
     </a>
-      <a href="/aboutme">
+      <a href="/aboutme" className="p-5">
         About Me
     </a>
-      <a href="/contact">
+      <a href="/contact" className="p-5">
         Contact
     </a>
     </div>
