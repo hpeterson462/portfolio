d6a2e721a223e2531382455fbfff473d7a3e1757
Heather Peterson <hpeterson462@gmail.com>
2020-12-10 14:38:36 -0800

    tech icon refactor, responsive

--- src/TechStack.jsx
+++ src/TechStack.jsx
@@ -17,20 +17,29 @@ export default function TechStack() {
       <br />
 
       <div className="flex flex-row flex-wrap items-center justify-center bg-gray-50 min-h-screen">
+
         <section className="p-5 flex items-center justify-around">
-          <div className="inset-0 mb-28">
-            HTML
+          <div className="h-52 w-52 relative cursor-pointer mb-5">
+            <div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl">
+              HTML
           </div>
-          <div className="absolute p-10 transform hover:translate-y-10 transition duration-300">
-            <div className="flex justify-center">
-              <DiHtml5 size={160} />
+            <div className="absolute inset-0 transform hover:translate-y-10 transition duration-300">
+              <div className="flex justify-center items-center h-full w-full bg-white rounded-lg shadow-2xl">
+                <DiHtml5 size={160} />
+              </div>
             </div>
           </div>
         </section>
 
-        <div className="p-10">
-          <DiJsBadge size={150} />
-        </div>
+        <section className="p-5 flex items-center justify-around">
+          <div className="inset-0 mb-28">
+            JavaScript
+          </div>
+          <div className="absolute p-20 transform hover:translate-y-10 transition duration-300">
+            <DiJsBadge size={150} />
+          </div>
+        </section>
+
         <div className="p-10">
           <DiReact size={150} />
         </div>
