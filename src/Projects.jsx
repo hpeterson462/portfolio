00a4cc7a88e2754e90f33d404c6ed254259b4329
Heather Peterson <hpeterson462@gmail.com>
2020-12-09 20:02:38 -0800

    boxes w/ place image & responsibe rows

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -9,20 +9,41 @@ export default function Projects() {
 
       <br />
 
-      <section className="p-10 min-h-screen flex items-center justify-around bg-lightBlue flex-wrap sm:flex-col">
-
-        <div className="h-48 w-48 relative cursor-pointer mb-5">
-          <div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl">
-            PageTrade
+      <main className="flex flex-row bg-lightBlue justify-evenly items-center">
+        <section className="p-10 min-h-screen flex items-center justify-around bg-lightBlue flex-wrap">
+          <div className="h-48 w-48 relative cursor-pointer mb-5">
+            <div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl">
+              PageTrade
           </div>
-          <div className="absolute inset-0 transform  hover:scale-75 transition duration-300">
-            <div className="h-full w-full bg-white rounded-lg shadow-2xl"></div>
+            <div className="absolute inset-0 transform  hover:scale-75 transition duration-300">
+              <div className="h-full w-full bg-white rounded-lg shadow-2xl bg-dots bg-center"></div>
+            </div>
           </div>
-        </div>
+        </section>
 
+        <section className="p-10 min-h-screen flex items-center justify-around bg-lightBlue flex-wrap">
+          <div className="h-48 w-48 relative cursor-pointer mb-5">
+            <div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl">
+              Sherlock Stocks
+          </div>
+            <div className="absolute inset-0 transform  hover:scale-75 transition duration-300">
+              <div className="h-full w-full bg-white rounded-lg shadow-2xl bg-dots bg-center"></div>
+            </div>
+          </div>
+        </section>
 
+        <section className="p-10 min-h-screen flex items-center justify-around bg-lightBlue flex-wrap">
+          <div className="h-48 w-48 relative cursor-pointer mb-5">
+            <div className="absolute inset-0 bg-white opacity-25 rounded-lg shadow-2xl">
+              Cumulus
+          </div>
+            <div className="absolute inset-0 transform  hover:scale-75 transition duration-300">
+              <div className="h-full w-full bg-white rounded-lg shadow-2xl bg-dots bg-center"></div>
+            </div>
+          </div>
+        </section>
+      </main>
 
-      </section>
     </div>
   )
 }
