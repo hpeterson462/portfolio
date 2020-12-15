f7708c9fb924578b0f0ed0aee5004594008959d5
Heather Peterson <hpeterson462@gmail.com>
2020-12-15 10:05:09 -0800

    text padding and line height about me page

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -8,12 +8,12 @@ export default function AboutMe() {
 
       <br />
       <div className="flex flex-row">
-        <div className="flex justify-center items-center bg-gray-50 leading-10 w-3/5 p-5 m-5 min-h-screen">
+        <div className="flex justify-center items-center bg-gray-50 leading-10 w-3/5 p-5 m-5 min-h-screen text-2xl">
           <p>
-            I am a <span className="text-lightBlue bg-darkGray italic p-2 rounded">full-stack</span> software developer with a passion for <span className="text-lightBlue bg-darkGray italic p-2 rounded">solving human-based problems</span>, collaborating with a great team, and the great outdoors. On previous projects, I’ve used <span className="text-lightBlue bg-darkGray italic p-2 rounded">test-driven development</span> to create <span className="text-lightBlue bg-darkGray italic p-2 rounded">PostgreSQL</span> databases using <span className="text-lightBlue bg-darkGray italic p-2 rounded">RESTful APIs</span> that distill information down into <span className="text-lightBlue bg-darkGray italic p-2 rounded">JavaScript</span> and  <span className="text-lightBlue bg-darkGray italic p-2 rounded">React</span> user interfaces with <span className="text-lightBlue bg-darkGray italic p-2 rounded">CSS</span> and <span className="text-lightBlue bg-darkGray italic p-2 rounded">Sass</span> designs. I am always <span className="text-lightBlue bg-darkGray italic p-2 rounded">curious</span> to continue to <span className="text-lightBlue bg-darkGray italic p-2 rounded">discover, learn, and grow</span> with amazing people.
+            I am a <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">full-stack</span> software developer with a passion for <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">solving human-based problems</span>, collaborating with a great team, and the great outdoors. On previous projects, I’ve used <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">test-driven development</span> to create <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">PostgreSQL</span> databases using <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">RESTful APIs</span> that distill information down into <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">JavaScript</span> and  <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">React</span> user interfaces with <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">CSS</span> and <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">Sass</span> designs. I am always <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">curious</span> to continue to <span className="text-lightBlue bg-darkGray italic p-0.5 rounded">discover, learn, and grow</span> with amazing people.
         </p>
         </div>
-        <div className="flex h-80 w-80 m-auto rounded">
+        <div className="flex h-90 w-80 m-auto rounded">
           <img src={heather} alt="Heather" />
         </div>
       </div>
