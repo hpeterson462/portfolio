0b9fe8110c671cfaff1ad3e3765987bf06cb1452
Heather Peterson <hpeterson462@gmail.com>
2020-12-21 15:51:35 -0800

    config update

--- craco.config.js
+++ craco.config.js
@@ -7,12 +7,4 @@ module.exports = {
       ],
     },
   },
-  eslint: {
-    enable: true,
-    mode: "extends" || "file",
-    configure: { /* Any eslint configuration options: https://eslint.org/docs/user-guide/configuring */ },
-    configure: (eslintConfig, { env, paths }) => { return eslintConfig; },
-    pluginOptions: { /* Any eslint plugin configuration options: https://github.com/webpack-contrib/eslint-webpack-plugin#options. */ },
-    pluginOptions: (eslintOptions, { env, paths }) => { return eslintOptions; }
-  },
 }
