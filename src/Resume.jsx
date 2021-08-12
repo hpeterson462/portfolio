279584437afc0e46481c86233348d6adeb48e409
Heather Peterson <hpeterson462@gmail.com>
2021-08-12 11:52:24 -0700

    refactor resume to function component

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -5,14 +5,18 @@ import PDF from './assets/resume.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const Resume = () => (
-  <Document
-    className='flex justify-center m-5'
-    file={PDF}
-    onContextMenu={(e) => e.preventDefault()}
-  >
-    <Page pageNumber={1} />
-  </Document>
-);
+export default function Resume() {
+  const handleWindow = (e) => {
+    e.preventDefault();
+  }
 
-export default Resume;
+  return (
+    <Document
+      className='flex justify-center m-5'
+      file={PDF}
+      onContextMenu={handleWindow}
+    >
+      <Page pageNumber={1} />
+    </Document>
+  )
+}
